862ccdf9cd1371228460a4f731da456faac9e688
GitHub Action <action@github.com>
2022-06-21 13:19:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/kew/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kew/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kew.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kew.myplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/kew/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kew/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kew.myplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kew.myplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
